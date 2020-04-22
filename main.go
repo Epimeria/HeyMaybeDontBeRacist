031849013bbd60b3994a8d4f045940a0b8d21a4d
Epimeria <61943481+Epimeria@users.noreply.github.com>
2020-04-22 15:22:44 -0500

    Fixed typo's inside the original copypasta, added a space inside the regex to prevent false-positives on military time.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/turnage/graw/reddit"
 )
 
-const copyPasta = `Haha, those sure are some kooky crime statistics you got there, dad! Where are those from, the FBI? Well, did you know that FBI crime statistics only track those that are successfully convicted of a crime, even though the vast majority of crimes committed aren't even reported, let alone lead to a successful arrest, prosecution, and conviction? Sounds like a big hole in the data if you ask me! And when you combine that with the fact that black neighbourhoods are more heavily policed black people are more likely to get stopped by the police, and more likely to be convicted by a jury for the same crimes as a white person, it makes you think that maybe those FBI stats have a lot more to do with systemic racial profiling than some sort of innate behaviour in black people! How's that for a brain blast?
+const copyPasta = `Haha, those sure are some kooky crime statistics you got there, dad! Where are those from, the FBI? Well, did you know that FBI crime statistics only track those that are successfully convicted of a crime, even though the vast majority of crimes committed aren't even reported, let alone lead to a successful arrest, prosecution, and conviction? Sounds like a big hole in the data if you ask me! And when you combine that with the fact that black neighborhoods are more heavily policed black people are more likely to get stopped by the police, and more likely to be convicted by a jury for the same crimes as a white person, it makes you think that maybe those FBI stats have a lot more to do with systemic racial profiling than some sort of innate behavior in black people! How's that for a brain blast?
 
 ^^This ^^action ^^was ^^performed ^^automatically
 `
@@ -25,7 +25,7 @@ type CommentHandler interface {
 }
 
 func (a *announcer) Comment(comment *reddit.Comment) error {
-	matched, _ := regexp.MatchString(`(13\%?\:?\;?\/? ?50\%?)( |$|\.|\,|\:|\;|\-)`, comment.Body)
+	matched, _ := regexp.MatchString(`(13\%?\:?\;?\/? ?50\%?)( |$|\.|\,|\: |\;|\-)`, comment.Body)
 	if matched {
 		fmt.Printf("%s was a little racist on %s: '%s'\n", comment.Author, comment.Subreddit, comment.Body)
 		return a.bot.Reply(comment.Name, copyPasta)
